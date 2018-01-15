93470680eae22a45395fe03d3e62aca0cfa70854
Jérémy Bourel <calaruni@hotmail.com>
2018-01-15 20:24:21 +0100

    add feature for admin

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,7 +12,7 @@ class DefaultController extends Controller
   /**
   * @Route("/", name="home")
   **/
-  public function home(UserInterface $user)
+  public function home()
   {
     return $this->render('home.html.twig');
   }
