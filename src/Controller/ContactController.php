7ca829a6d5dd4764116e75380982106ad69d2fab
Jérémy Bourel <calaruni@hotmail.com>
2018-01-25 21:20:14 +0100

    add contact : create entity

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+class ContactController extends Controller
+{
+  /**
+  * @Route("/contact", name="contact")
+  **/
+  public function showAction(){
+    
+      return $this->render('contact/index.html.twig');
+  }
+}
---
+++ src/Entity/Contact.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ContactRepository")
+ */
+class Contact
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+    * @ORM\Column(type="string", length=100)
+    */
+    private $firstname;
+
+    /**
+    * @ORM\Column(type="string", length=100)
+    */
+    private $lastname;
+
+    /**
+    * @ORM\Column(type="string", length=320)
+    */
+    private $mail;
+
+    /**
+    * @ORM\Column(type="text")
+    */
+    private $message;
+
+    /**
+    * Return id of the message
+    *
+    * @return int
+    **/
+    public function getId()
+    {
+      return $this->id;
+    }
+
+    /**
+    * Set the id of message
+    *
+    * @var Int
+    *
+    * @return Int
+    */
+    public function setId(int $id)
+    {
+      $this->id = $id;
+      return $this;
+    }
+
+    /**
+    * Return firstname of the user who has send the message
+    *
+    * @return string
+    */
+    public function getFirstname()
+    {
+      return $this->firstname;
+    }
+
+    /**
+    * Set the firstname of the user who has send the message
+    *
+    * @var String
+    *
+    * @return String
+    **/
+    public function setFirstname(String $firstname)
+    {
+      $this->firstname = $firstname;
+      return $this;
+    }
+
+    /**
+    * Return the lastname of the user who has send the message
+    *
+    * @return String
+    */
+    public function getLastname()
+    {
+      return $this->lastname;
+    }
+
+    /**
+    * Set the lastname of the user who has send the messages
+    *
+    * @var String
+    *
+    * @return String
+    **/
+    public function setLastname(string $lastname)
+    {
+      $this->lastname = $lastname;
+      return $this;>
+    }
+
+    /**
+    * Return the message
+    *
+    * @return String
+    */
+    public function getMessage()
+    {
+      return $this->message;
+    }
+
+    /**
+    * Set the message
+    *
+    * @var String
+    *
+    * @return String
+    **/
+    public function setMessage(string $message)
+    {
+      $this->message = $message;
+      return $this;
+    }
+
+}
