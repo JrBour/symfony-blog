9e539a8243b6afe9760873c9ede494aa02cd170d
Jérémy Bourel <calaruni@hotmail.com>
2018-02-01 09:17:37 +0100

    edit comments : finish entity

--- src/Entity/Comments.php
+++ src/Entity/Comments.php
@@ -24,15 +24,108 @@ class Comments
     private $content;
 
     /**
-    * @ORM\ManyToOne(targetEntity="App\Entity\Blog", inversedBy="comments")
+    * @ORM\OneToOne(targetEntity="App\Entity\Blog", inversedBy="comments")
     * @ORM\JoinColumn(nullable=true)
     **/
     private $blog;
 
     /**
-    * @ORM\ManyToOne(targetEntity="App\Entity\User", inversedBy="comments")
+    * @ORM\OneToOne(targetEntity="App\Entity\User", inversedBy="comments")
     * @ORM\JoinColumn(nullable=true)
     **/
     private $author;
 
+    /**
+    * Return id
+    *
+    * @return int
+    **/
+
+    public function getId()
+    {
+      return $this->id;
+    }
+
+    /**
+    * Set the comment id
+    *
+    * @var Int
+    *
+    * @return Int
+    **/
+    public function setId(int $id)
+    {
+      $this->id = $id;
+      return $this;
+    }
+
+    /**
+    * Return the content
+    *
+    * @return String
+    **/
+    public fucntion getContent()
+    {
+      return $this->content;
+    }
+
+    /**
+    * Set the content
+    *
+    * @var String
+    *
+    * @return String
+    **/
+    public function setContent(string $content)
+    {
+      $this->content = $content;
+      return $this->content;
+    }
+
+    /**
+    * Return the blog
+    *
+    * @return String
+    **/
+    public function getBlog()
+    {
+      $this->blog = $blog;
+    }
+
+    /**
+    * Set the blog
+    *
+    * @var String
+    *
+    * @return String
+    **/
+    public function setBlog(Blog $blog)
+    {
+      $this->blog = $blog;
+      return $this;
+    }
+
+    /**
+    * Get the author
+    *
+    * @return String
+    **/
+    public function getAuthor()
+    {
+      return $this->author;
+    }
+
+    /**
+    * Set the author
+    *
+    * @var String
+    *
+    * @return String
+    **/
+    public function setAuthor(User $author)
+    {
+      $this->author = $author;
+
+      return $this->author;
+    }
 }
