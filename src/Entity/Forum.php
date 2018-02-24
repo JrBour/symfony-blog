7046d1ee19ac14ce59d8548456f2c1b5bfeeaba2
Jérémy Bourel <calaruni@hotmail.com>
2018-02-24 19:30:12 +0100

    add entity for forum

---
+++ src/Entity/Forum.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ForumRepository")
+ */
+class Forum
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+    * @ORM\Column(type="string", length="200")
+    **/
+    private $title;
+
+    /**
+    * @ORM\Column(type="text")
+    **/
+    private $content;
+
+    /**
+    * @ORM\Column(type="datetime")
+    **/
+    private $created_at;
+
+    /**
+    * @ORM\Column(type="datetime", nullable=true)
+    **/
+    private $updated_at;
+
+    /**
+    * Return the title
+    *
+    * @return String
+    **/
+    public function getTitle()
+    {
+      return $this->title;
+    }
+
+    /**
+    * Set the title
+    *
+    * @var String
+    *
+    * @return String
+    **/
+    public function setTitle(string $title)
+    {
+      $this->title = $title;
+      return $this;
+    }
+
+    /**
+    * Get the content
+    *
+    * @return String
+    **/
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+    * Set the content
+    *
+    * @var String
+    *
+    * @return String
+    **/
+    public function setTitle(string $content)
+    {
+        $this->content = $content;
+        return $this;
+    }
+
+    /**
+    * Get the date where the question was created
+    *
+    * @return Datetime
+    **/
+    public function getCreatedAt()
+    {
+      return $this->created_at;
+    }
+
+    /**
+    * Set the date where the questions was created
+    *
+    * @var Datetime
+    *
+    * @return Datetime
+    **/
+    public function setCreatedAt(Datetime $createdAt)
+    {
+      $this->created_at = $createdAt;
+      return $this;
+    }
+
+    /**
+    * Get the update date
+    *
+    * @return Datetime
+    **/
+    public function getUpdatedAt()
+    {
+      return $this->updated_at;
+    }
+
+    /**
+    * Set the update date
+    *
+    * @var Datetime
+    *
+    * @return Datetime
+    **/
+    public function setUpdatedAt(Datetime $updatedAt)
+    {
+      $this->updated_at = $updatedAt;
+      return $this;
+    }
+
+}
---
+++ src/Repository/ForumRepository.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Forum;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+/**
+ * @method Forum|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Forum|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Forum[]    findAll()
+ * @method Forum[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class ForumRepository extends ServiceEntityRepository
+{
+    public function __construct(RegistryInterface $registry)
+    {
+        parent::__construct($registry, Forum::class);
+    }
+
+    /*
+    public function findBySomething($value)
+    {
+        return $this->createQueryBuilder('f')
+            ->where('f.something = :value')->setParameter('value', $value)
+            ->orderBy('f.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+}
