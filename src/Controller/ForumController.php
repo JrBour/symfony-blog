5936df4d8cb6d0f5b8788b4f6e043b640a98555b
Jérémy Bourel <calaruni@hotmail.com>
2018-02-25 17:50:14 +0100

    add controller for new forum

--- src/Controller/ForumController.php
+++ src/Controller/ForumController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Form\ForumType;
+use App\Entity\Forum;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -15,5 +19,35 @@ class ForumController extends Controller
         return $this->render('forum/index.html.twig', [
             'controller_name' => 'ForumController',
         ]);
+    }
+
+    /**
+    * @Route("/forum/new", name="forum_add")
+    **/
+    public function addForumAction(Request $request)
+    {
+      $em = $this->getDoctrine()->getManager();
+      $forum = $em->getRepository(Forum::class)->findAll();
+
+      $forum = new Forum();
+      $form = $this->createForm(ForumType::class, $forum);
+      $form->handleRequest();
+
+      if($form->isSubmitted() && $form->isValid()){
+        $form->getData();
+
+        $forum->setTitle($forum->getTitle());
+        $forum->setContent($forum->getContent());
+        $dateNow = new DateTime(date('Y-m-d H:i:s'));
+        $forum->setCreatedAt($dateNow);
+
+        $em->persist($forum);
+        $em->flush();
+      }
+
+      return $this->render('forum/new.html.twig',
+        array(
+          'form' => $form->createView()
+       ))
     }
 }
