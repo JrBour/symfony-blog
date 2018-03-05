9729e8b1002067e8f5c3655ca62e0135ff11db1e
Jérémy Bourel <calaruni@hotmail.com>
2018-03-05 22:12:32 +0100

    edit answer : add list answers

--- src/Controller/AnswerController.php
+++ src/Controller/AnswerController.php
@@ -4,16 +4,20 @@ namespace App\Controller;
 
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use App\Entity\Answer;
 
 class AnswerController extends Controller
 {
     /**
      * @Route("/answer", name="answer")
      */
-    public function index()
+    public function listAnswerAction()
     {
-        return $this->render('answer/index.html.twig', [
-            'controller_name' => 'AnswerController',
-        ]);
+        $answers = $this->getDoctrine()->getManager()->getRepository(Answer::class)->findAll();
+
+        return $this->render('answer/index.html.twig',
+          array(
+            'answers' => $answers
+        ));
     }
 }
