cca29b2186c2abc39ad7a12d64f8890f56949e11
Jérémy Bourel <calaruni@hotmail.com>
2018-05-15 12:06:10 +0200

    refactoring : edit the name route

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -20,159 +20,155 @@ use \DateTime;
 
 class BlogController extends Controller
 {
-  /**
-  * @Route("/blog", name="blog")
-  **/
-  public function index(BlogRepository $blog, CategoryRepository $category): Response
-  {
-    return $this->render('blog/index.html.twig', ['posts' => $blog->findAll(), 'categories' => $category->findAll()]);
-  }
-
-  /**
-  * @Route("/blog/add", name="blog_add")
-  **/
-  public function add(Request $request)
-  {
-    $em = $this->getDoctrine()->getManager();
-    $categories = $this->getDoctrine()
-        ->getRepository(Category::class)
-        ->findAll();
-    $post = new Blog();
-    $form = $this->createForm(BlogType::class, $post, array( 'choices' => $categories));
-    $form->handleRequest($request); // Permet de manipuler la requête
-
-    if ($form->isSubmitted() && $form->isValid()) {
-      $post = $form->getData();
-      $user = $this->getUser();
-      $file = $post->getImage();
-      $fileName = md5(uniqid()) . '.' . $file->guessExtension();
-      $file->move(
-        $this->getParameter('images'),
-        $fileName
-      );
-      $name = "/images/posts/" . $fileName;
-      $post->setImage($name);
-      $post->setTitle($post->getTitle());
-      $post->setDescription($post->getDescription());
-      $date = new DateTime(date("Y-m-d H:i:s"));
-      $post->setDate($date);
-      $post->setCategory($post->getCategory());
-      $post->setAuthor($user);
-
-      $em->persist($post); // Query : INSERT INTO …
-      $em->flush(); // Send the query
-
-      return $this->redirectToRoute('blog');
+    /**
+     * @Route("/blog", name="blog")
+     **/
+    public function index(BlogRepository $blog, CategoryRepository $category): Response
+    {
+        return $this->render('blog/index.html.twig', ['posts' => $blog->findAll(), 'categories' => $category->findAll()]);
     }
 
-    return $this->render('blog/add.html.twig', array(
-      'form' => $form->createView(),
-      'categories' => $categories
-    ));
-  }
-  /**
-  * @Route("/blog/{id}", name="show_id")
-  **/
-  public function showPost(Request $request, int $id)
-  {
-    $em = $this->getDoctrine()->getManager();
-    $blog = $this->getDoctrine()
-      ->getRepository(Blog::class)
-      ->find($id);
-    $comments = $this->getDoctrine()
-              ->getRepository(Comment::class)
-              ->findByPost($id);
+    /**
+     * @Route("/blog/add", name="blog_post")
+     **/
+    public function post(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $categories = $this->getDoctrine()
+            ->getRepository(Category::class)
+            ->findAll();
+        $post = new Blog();
+        $form = $this->createForm(BlogType::class, $post, array( 'choices' => $categories));
+        $form->handleRequest($request); // Permet de manipuler la requête
 
-    $comment = new Comment();
-    $form = $this->createForm(CommentType::class, $comment);
-    $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $post = $form->getData();
+            $user = $this->getUser();
+            $file = $post->getImage();
+            $fileName = md5(uniqid()) . '.' . $file->guessExtension();
+            $file->move(
+                $this->getParameter('images'),
+                $fileName
+            );
+            $name = "/images/posts/" . $fileName;
+            $post->setImage($name);
+            $post->setTitle($post->getTitle());
+            $post->setDescription($post->getDescription());
+            $date = new DateTime(date("Y-m-d H:i:s"));
+            $post->setDate($date);
+            $post->setCategory($post->getCategory());
+            $post->setAuthor($user);
 
-    if ($request->isXmlHttpRequest()) {
-      $data = $request->request->all();
-      $data['user'] = $this->getUser();
-      $data['id'] = $data['user']->getId();
-      $data['username'] = $data['user']->getUsername();
-      $data['image'] = $data['user']->getImage();
-      $date = new DateTime(date("Y-m-d H:i:s"));
+            $em->persist($post); // Query : INSERT INTO …
+            $em->flush(); // Send the query
 
-      $comment->setContent($data['content']);
-      $comment->setAuthor($data['user']);
-      $comment->setBlog($blog);
-      $comment->setDate($date);
+            return $this->redirectToRoute('blog');
+        }
 
-      $em->persist($comment);
-      $em->flush();
+        return $this->render('blog/add.html.twig', array(
+            'form' => $form->createView(),
+            'categories' => $categories
+        ));
+    }
+    /**
+     * @Route("/blog/{id}", name="blog_show")
+     **/
+    public function show(Request $request, int $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $blog = $this->getDoctrine()
+            ->getRepository(Blog::class)
+            ->find($id);
+        $comments = $this->getDoctrine()
+            ->getRepository(Comment::class)
+            ->findByPost($id);
 
-      return new JsonResponse($data, 200);
+        $comment = new Comment();
+        $form = $this->createForm(CommentType::class, $comment);
+        $form->handleRequest($request);
+
+        if ($request->isXmlHttpRequest()) {
+            $data = $request->request->all();
+            $data['user'] = $this->getUser();
+            $data['id'] = $data['user']->getId();
+            $data['username'] = $data['user']->getUsername();
+            $data['image'] = $data['user']->getImage();
+            $date = new DateTime(date("Y-m-d H:i:s"));
+
+            $comment->setContent($data['content']);
+            $comment->setAuthor($data['user']);
+            $comment->setBlog($blog);
+            $comment->setDate($date);
+            $em->persist($comment);
+            $em->flush();
+
+            return new JsonResponse($data, 200);
+        }
+
+        return $this->render('blog/show.html.twig', array(
+            'blog' => $blog,
+            'form' => $form->createView(),
+            'comments' => $comments
+        ));
     }
 
-    return $this->render('blog/show.html.twig', array(
-      'blog' => $blog,
-      'form' => $form->createView(),
-      'comments' => $comments
-    ));
-  }
+    /**
+     * @Route("/blog/edit/{id}", name="blog_edit")
+     **/
+    public function edit(Request $request, int $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $currentBlog = $this->getDoctrine()
+            ->getRepository(Blog::class)
+            ->find($id);
+        $categories = $this->getDoctrine()
+            ->getRepository(Category::class)
+            ->findAll();
+        $post = $em->getRepository(Blog::class)->find($id);
 
-  /**
-  * @Route("/blog/edit/{id}", name="edit_post")
-  **/
-  public function editPost(Request $request, int $id)
-  {
-    $em = $this->getDoctrine()->getManager();
-    $currentBlog = $this->getDoctrine()
-        ->getRepository(Blog::class)
-        ->find($id);
-    $categories = $this->getDoctrine()
-        ->getRepository(Category::class)
-        ->findAll();
-    $post = $em->getRepository(Blog::class)->find($id);
+        $form = $this->createForm(BlogType::class, $post, [ 'choices' => $categories ]);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $post = $form->getData();
+            $file = $post->getImage();
+            if ($file) {
+                $fileName = md5(uniqid()) . '.' . $file->guessExtension();
+                $file->move(
+                    $this->getParameter('images'),
+                    $fileName
+                );
+                $name = "/images/posts/" . $fileName;
+                $post->setImage($name);
+            } else {
+                $post->setImage($currentBlog->getImage());
+            }
+            $post->setCategory($post->getCategory());
+            $post->setTitle($post->getTitle());
+            $post->setDescription($post->getDescription());
+            $em->flush();
 
-    $form = $this->createForm(BlogType::class, $post, array( 'choices' => $categories ));
-    $form->handleRequest($request);
-    if ($form->isSubmitted() && $form->isValid()) {
-      $post = $form->getData();
-      $file = $post->getImage();
-      if ($file) {
-        $fileName = md5(uniqid()) . '.' . $file->guessExtension();
-        $file->move(
-          $this->getParameter('images'),
-          $fileName
-        );
-        $name = "/images/posts/" . $fileName;
-        $post->setImage($name);
-      } else {
-        $post->setImage($currentBlog->getImage());
-      }
+            return $this->redirectToRoute('blog');
+        }
 
-      $post->setCategory($post->getCategory());
-      $post->setTitle($post->getTitle());
-      $post->setDescription($post->getDescription());
-
-      $em->flush();
-
-      return $this->redirectToRoute('blog');
+        return $this->render('blog/add.html.twig', array(
+            'title' => 'edit',
+            'form' => $form->createView(),
+            'blog' => $post
+        ));
     }
 
-    return $this->render('blog/add.html.twig', array(
-      'title' => 'edit',
-      'form' => $form->createView(),
-      'blog' => $post
-    ));
-  }
+    /**
+     * @Route("/blog/delete/{id}", name="blog_remove")
+     **/
+    public function removePost(int $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $post = $em->getRepository(Blog::class)->find($id);
+        $em->remove($post);
+        $em->flush();
 
-  /**
-  * @Route("/blog/delete/{id}", name="remove_post")
-  **/
-  public function removePost(int $id)
-  {
-    $em = $this->getDoctrine()->getManager();
-    $post = $em->getRepository(Blog::class)->find($id);
-
-    $em->remove($post);
-    $em->flush();
-
-    return $this->redirectToRoute('blog');
-  }
+        return $this->redirectToRoute('blog');
+    }
 }
 
- ?>
+?>
