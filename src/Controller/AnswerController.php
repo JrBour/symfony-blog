4db50e676d7dfca5ef8fbe03b55aa562fadc70db
Jérémy Bourel <calaruni@hotmail.com>
2018-05-16 19:03:54 +0200

    refactoring : edit answer controller

--- src/Controller/AnswerController.php
+++ src/Controller/AnswerController.php
@@ -11,6 +11,9 @@ use App\Entity\Answer;
 class AnswerController extends Controller
 {
     /**
+     * Return all answers in dashboard for the admin
+     * @param AnswerRepository      $answerRepository       Repository of the answer part
+     * @return      Response        The response send a view in twig
      * @Route("/answer", name="answer")
      */
     public function index(AnswerRepository $answerRepository): Response
@@ -19,13 +22,16 @@ class AnswerController extends Controller
     }
 
     /**
-    * @Route("/answer/delete/{id}/{idForum}", name="answer_remove")
-    */
+     * Remove one answer select by his id
+     * @param int       $id         The answer id
+     * @param int       $idForum    The forum id
+     * @return Response     The response send a view in twig
+     * @Route("/answer/delete/{id}/{idForum}", name="answer_remove")
+     */
     public function removeAnswerAction(int $id, int $idForum): Response
     {
       $em = $this->getDoctrine()->getManager();
       $answer = $em->getRepository(Answer::class)->find($id);
-
       $em->remove($answer);
       $em->flush();
 
