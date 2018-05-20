ff498c656239ac9392d7e6b12ef9d79ddb5b23d8
Jérémy Bourel <calaruni@hotmail.com>
2018-05-20 20:19:15 +0200

    refactoring : edit role entity

--- src/Entity/Role.php
+++ src/Entity/Role.php
@@ -13,6 +13,8 @@ use Doctrine\Common\Collections\Collection;
 class Role
 {
     /**
+     * Role id
+     * @var int
      * @ORM\Id
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
@@ -20,35 +22,55 @@ class Role
     private $id;
 
     /**
+     * Role name
+     * @var string
     * @ORM\Column(type="string")
     */
     private $name;
 
     /**
+     * User role
+     * @var User
     * @ORM\OneToMany(targetEntity="App\Entity\User", mappedBy="role")
     **/
     private $user;
 
+    /**
+     * Role constructor.
+     */
     public function __construct()
     {
       $this->user = new ArrayCollection();
     }
 
+    /**
+     * @return int
+     */
     public function getId()
     {
       return $this->id;
     }
 
+    /**
+     * @param int $id
+     */
     public function setId(int $id)
     {
       $this->id = $id;
     }
 
+    /**
+     * @return string
+     */
     public function getName()
     {
       return $this->name;
     }
 
+    /**
+     * @param string $name
+     * @return string
+     */
     public function setName(string $name)
     {
       $this->name = $name;
