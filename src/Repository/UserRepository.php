54922d9f6466b97a90d1147cdd464c95f7cc54c2
Jérémy Bourel <calaruni@hotmail.com>
2018-06-04 23:10:55 +0200

    Refactoring : User repository

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -8,11 +8,19 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
 
 class UserRepository extends ServiceEntityRepository
 {
+    /**
+     * UserRepository constructor.
+     * @param RegistryInterface $registry
+     */
     public function __construct(RegistryInterface $registry)
     {
         parent::__construct($registry, User::class);
     }
 
+    /**
+     * @param int $id
+     * @return mixed
+     */
     public function findByCategory(int $id)
     {
         return $this->createQueryBuilder('b')
