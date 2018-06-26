e16dc89c70e3bc9b67d5110ed3684739f842c0e8
Jérémy Bourel <calaruni@hotmail.com>
2018-06-26 21:46:13 +0200

    edit room : add setter

--- src/Entity/Room.php
+++ src/Entity/Room.php
@@ -72,6 +72,25 @@ class Room
     }
 
     /**
+     * Get the room picture
+     * @return String
+     */
+    public function getPicture(): String
+    {
+        return $this->picture;
+    }
+
+    /**
+     * Set the room picture
+     * @param String $picture
+     * @return Void
+     */
+    public function setPicture(String $picture): void
+    {
+        $this->picture = $picture;
+    }
+
+    /**
      * Get the date when the room created
      * @return DateTime
      */
@@ -83,21 +102,12 @@ class Room
     /**
      * Set the date when the room created
      * @param DateTime $created_at
+     * @return Void
      */
     public function setCreatedAt(DateTime $created_at): void
     {
         $this->created_at = $created_at;
     }
-
-    /**
-     * Get the room picture
-     * @return String
-     */
-    public function getPicture(): String
-    {
-        return $this->picture;
-    }
-
 
     /**
      * Get the date when the room updated
@@ -106,5 +116,14 @@ class Room
     public function getUpdatedAt(): DateTime
     {
         return $this->updated_at;
+    }
+
+    /**
+     * Set the date when the room updated
+     * @param DateTime $updated_at
+     */
+    public function setUpdatedAt(DateTime $updated_at): void
+    {
+        $this->updated_at = $updated_at;
     }
 }
