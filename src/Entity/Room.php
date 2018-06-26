74a2bd2533618e3f4f057e56e025376411ec9fcf
Jérémy Bourel <calaruni@hotmail.com>
2018-06-26 21:32:45 +0200

    edit room : add picture and created_at variable

--- src/Entity/Room.php
+++ src/Entity/Room.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use \DateTime;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\RoomRepository")
@@ -17,20 +18,46 @@ class Room
     private $id;
 
     /**
+     * @var String
      * @ORM\Column(type="string", length=255)
      */
     private $name;
 
+    /**
+     * @var String
+     * @ORM\Column(type="string", length=255)
+     */
+    private $picture;
+
+    /**
+     * @var DateTime
+     * @ORM\Column(type="date")
+     */
+    private $created_at;
+
+    /**
+     * Get the room id
+     * @return mixed
+     */
     public function getId()
     {
         return $this->id;
     }
 
+    /**
+     * Get the room name
+     * @return null|string
+     */
     public function getName(): ?string
     {
         return $this->name;
     }
 
+    /**
+     * Set the room name
+     * @param   string    $name     The room name
+     * @return Room
+     */
     public function setName(string $name): self
     {
         $this->name = $name;
