f447f80347a0a92b14f4fd892feb6671142a1d59
Jérémy Bourel <calaruni@hotmail.com>
2018-09-03 20:57:29 +0200

    edit meesage : edit the method for create new message

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -21,38 +22,32 @@ class MessageController extends Controller
     /**
      * @param Request $request
      * @return \Symfony\Component\HttpFoundation\JsonResponse
+     * @Route("/message/create", name="message_create")
      */
     public function create(Request $request): Response
     {
-        if($request->isXmlHttpRequest()) {
-            $room = new Room();
-            $data = $request->attributes->all();
-            $em = $this->getDoctrine()->getManager();
-
-            $room->setTitle($data['name']);
-            $room->setPicture($data['picture']);
-            $room->setCreatedAt(new \DateTime());
-
-            $em->persist($room);
-            $em->flush();
-            $data['success'] = "The room have been created with the name" . $room['name'];
-
-            return $this->json($data, 201);
-        }
-
         if ($request->isXmlHttpRequest()) {
             $message = new Message();
             $data = $request->attributes->all();
             $em = $this->getDoctrine()->getManager();
 
+            $recipient = $em->getRepository(User::class)->find($data['recipient']);
+            $sender = $em->getRepository(User::class)->find($data['sender']);
+
             $message->setContent($data['content']);
-            $message->setRecipientId($data['recipient']);
-            $message->setSenderId($data['sender']);
+            $message->setRecipientId($recipient);
+            $message->setSenderId($sender);
             $message->setCreatedAt(new \DateTime());
             $message->setRoomId($data['room']);
 
+            // Add in the queue
             $em->persist($message);
+            // Told  the database that he had to create\update\delete this information
             $em->flush();
+
+            return $this->json(['success' => 'Le message a bien été crée !'], 201);
         }
+
+        return $this->json(['error' => 'Veuillez effectuez une requête ajax !'], 403);
     }
 }
