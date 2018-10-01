34025347e9793fd3d97fcad8f8e772d66315047a
Jérémy Bourel <calaruni@hotmail.com>
2018-10-01 21:57:53 +0200

    edit room : style the message

--- src/Form/MessageType.php
+++ src/Form/MessageType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Message;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,7 +14,12 @@ class MessageType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('content')
+            ->add('content', TextType::class, [
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Type a message…'
+                ]
+            ])
             ->add('submit', SubmitType::class, ['label' => 'Send'])
         ;
     }
